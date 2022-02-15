9d8548cf9bc7b0e05b8f7b91236a18e37290263d
manick02 <manickavasagam.sundaram@gmail.com>
2022-02-15 17:04:23 +0530

    Setting up curator

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,7 +1,11 @@
-package demo;
-
+import org.apache.curator.test.TestingServer;
 public class DemoApplication {
+    /**
+     * DemoApplication
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        TestingServer server = new TestingServer();
     }
 }
